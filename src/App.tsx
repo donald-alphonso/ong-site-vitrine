b227007fc7213b2abce23b9085038ddd612b1c58
donald.ahossi <donaldalphonso11@gmail.com>
2024-12-03 08:52:54 +0100

    Add hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { Hero } from './components/Hero';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
       <Footer />
     </div>
   );
