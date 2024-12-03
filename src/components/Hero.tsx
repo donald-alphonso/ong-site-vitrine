e208a9f2c8ec4ab626fd6fd44260bc0d1c876317
donald.ahossi <donaldalphonso11@gmail.com>
2024-12-03 09:00:54 +0100

    Format file with prettier

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Hero() {
   return (
-    <div id="home" className='relative h-screen'>
+    <div id="home" className="relative h-screen">
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{
@@ -12,10 +12,12 @@ export function Hero() {
       >
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       </div>
-      <div className='relative h-full flex items-center justify-center text-center px-4'>
-        <div className='max-w-3xl'>
-          <h1 className='text-4xl md:text-6xl font-bold text-white mb-6'>Donnons de l'espoir aux Enfants</h1>
-          <p className='text-xl text-white mb-8'>
+      <div className="relative h-full flex items-center justify-center text-center px-4">
+        <div className="max-w-3xl">
+          <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">
+            Donnons de l'espoir aux Enfants
+          </h1>
+          <p className="text-xl text-white mb-8">
             Ensemble, construisons un avenir meilleur pour les orphelins à
             travers l'amour, l'éducation et le soutien.
           </p>
