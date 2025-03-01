5a0e3c6c5e259f9ffb152186db7d265a1ecd2b37
donald.ahossi <donaldalphonso11@gmail.com>
2025-03-01 12:33:57 +0100

    Séparation du front-end et du backend

--- server/src/routes/contactRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createContact } from '../controllers/contactController';
-
-const router = express.Router();
-
-router.post('/contact', createContact);
-
-export default router;
